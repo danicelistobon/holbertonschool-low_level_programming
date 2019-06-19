e5fdd9e5fc7282558c6ba64b683a9bc2e601b304
daniceliston <danielct-97@hotmail.com>
2019-06-19 22:26:28 +0000

    add 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of an long init: %d byte(s)\n", sizeof(long));
-printf("Size of an long long init: %d byte(s)\n", sizeof(long long));
-printf("Size of an float: %d byte(s)\n", sizeof(float));
+printf("Size of a long init: %d byte(s)\n", sizeof(long));
+printf("Size of a long long init: %d byte(s)\n", sizeof(long long));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
