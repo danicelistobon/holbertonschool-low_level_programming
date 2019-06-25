fa9e2b010078b9732e32461b4f5def32e7222daf
daniceliston <danielct-97@hotmail.com>
2019-06-25 19:22:33 +0000

    add

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,7 +11,7 @@ if (c >= 'a' && c <= 'z')
 {
 	return (1);
 }
-if (c >= 'a' && c <= 'z')
+else if (c >= 'A' && c <= 'Z')
 {
 	return (1);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 int _putchar(char c);
 void print_alphabet(void);
