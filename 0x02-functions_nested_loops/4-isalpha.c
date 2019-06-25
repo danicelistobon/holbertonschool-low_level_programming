bc3868b912b02d6f91ffdaab099912df950703a8
daniceliston <danielct-97@hotmail.com>
2019-06-25 18:59:38 +0000

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _isalpha - Checks for alphabetic character
+ * @c: character
+ * Return: return 1 if is a letter or 0 otherwise
+ */
+int _isalpha(int c)
+{
+
+if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+_putchar('\n');
+}
