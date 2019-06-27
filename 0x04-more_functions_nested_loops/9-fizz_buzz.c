3ce47b2d15d2021d3dce2efbabfb84ed99aef720
daniceliston <danielct-97@hotmail.com>
2019-06-27 21:15:33 +0000

    add 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 1; num <= 100; num++)
+{
+	if ((num % 15) == 0)
+	{
+		printf("FizzBuzz");
+		printf(" ");
+	}
+	else if ((num % 3) == 0)
+	{
+		printf("Fizz");
+		printf(" ");
+	}
+	else if ((num % 5) == 0)
+	{
+		printf("Buzz");
+		printf(" ");
+	}
+	else
+	{
+		printf("%d", num);
+		printf(" ");
+	}
+}
+printf("\n");
+return (0);
+}
