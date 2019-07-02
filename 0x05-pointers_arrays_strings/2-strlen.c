062f5109c0ea863e5c6bf01de7063c0db0d04cf8
daniceliston <danielct-97@hotmail.com>
2019-07-02 17:06:10 +0000

    add 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: pointer
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
