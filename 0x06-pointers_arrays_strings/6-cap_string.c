98699985eedbb2b564db7f4a7b07abe7afd1a73e
daniceliston <danielct-97@hotmail.com>
2019-07-05 00:08:52 +0000

    add 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * *cap_string - capitalizes all words of a string
+ *
+ * @cs: pointer
+ *
+ * Return: string
+ */
+char *cap_string(char *cs)
+{
+	int c, k;
+
+	c = 0;
+
+	while (*(cs + c) != '\0')
+	{
+		if (*(cs + c) == ' ' || *(cs + c) == '\t' || *(cs + c) == '\n'
+		|| *(cs + c) == ',' || *(cs + c) == ';' || *(cs + c) == '.'
+		|| *(cs + c) == '!' || *(cs + c) == '?' || *(cs + c) == '"'
+		|| *(cs + c) == '(' || *(cs + c) == ')' || *(cs + c) == '{'
+		|| *(cs + c) == '}')
+		{
+			k = 1;
+		}
+		else if (*(cs + c) >= 97 && *(cs + c) <= 122 && k == 1)
+		{
+			*(cs + c) -= 32;
+			k = 0;
+		}
+		else
+		{
+			k = 0;
+		}
+		c++;
+	}
+	return (cs);
+}
