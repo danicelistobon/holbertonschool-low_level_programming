c0f9424a42df1e03d89d18edfba33889bd2d8a4b
daniceliston <danielct-97@hotmail.com>
2019-07-08 23:38:41 +0000

    add 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * *_strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: pointer
+ *
+ * @accept: character
+ *
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int c, d;
+
+	c = 0;
+	d = 0;
+
+	for (c = 0; *(s + c) > ' '; c++)
+	{
+		for (d = 0; *(accept + d) != '\0'; d++)
+		{
+			if (*(accept + d) == *(s + c))
+			{
+				return (s + c);
+			}
+		}
+	}
+	return (0);
+}
