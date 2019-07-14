443effd5a5ac416e569c189ec7f6aa1b508cb6ce
danicelistobon <danielct-97@hotmail.com>
2019-07-14 03:26:22 +0000

    add 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: number of things in the command line
+ *
+ * @argv: array of string values
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+
+	c = 0;
+
+	while (c < argc)
+	{
+		printf("%s\n", *(argv + c));
+		c++;
+	}
+	return (0);
+}
