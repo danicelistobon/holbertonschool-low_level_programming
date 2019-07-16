b123557abbe591f6f00ac02ef65a48c884702d78
daniceliston <danielct-97@hotmail.com>
2019-07-16 14:52:15 +0000

    add 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins to make change
+ * for an amount of money
+ *
+ * @argc: number of things in the command line
+ *
+ * @argv: array of string values
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c, n;
+
+	c = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n = atoi(*(argv + 1));
+
+	if (n < 0)
+	{
+		printf("%d\n", c);
+		return (c);
+	}
+
+	c = c + (n / 25);
+	n = n % 25;
+	c = c + (n / 10);
+	n = n % 10;
+	c = c + (n / 5);
+	n = n % 5;
+	c = c + (n / 2);
+	n = n % 2;
+	c = c + n;
+
+	printf("%d\n", c);
+	return (c);
+}
