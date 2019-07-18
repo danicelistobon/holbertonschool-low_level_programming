c66afe9c8369491ebdc2f963df4ecbbd8c2fb66a
danicelistobon <danielct-97@hotmail.com>
2019-07-18 18:55:20 +0000

    add 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *malloc_checked - allocates memory using malloc
+*
+* @b: unsigned int given
+*
+* Return: 0
+*/
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
