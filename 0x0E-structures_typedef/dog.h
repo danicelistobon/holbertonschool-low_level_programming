e0c1f3900436261f0887c5f65778c785654d96ee
daniceliston <danielct-97@hotmail.com>
2019-07-22 22:22:09 +0000

    add

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+* struct dog - information of a dog
+*
+* @name: dog name
+*
+* @age: dog age
+*
+* @owner: dog owner
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
