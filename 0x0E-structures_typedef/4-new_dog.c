e155bbdb6f9737ed6c48584f871872ed37780747
danicelistobon <danielct-97@hotmail.com>
2019-07-23 02:19:51 +0000

    add 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+* _strlen - returns the length of a string
+*
+* @s: pointer
+*
+* Return: length
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+* *_strcpy - copies the string pointed
+*
+* @dest: pointer
+*
+* @src: pointer
+*
+* Return: string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int c;
+
+	for (c = 0; *(src + c) != '\0'; c++)
+	{
+		*(dest + c) = *(src + c);
+	}
+	*(dest + c) = '\0';
+
+	return (dest);
+}
+
+/**
+* *new_dog - creates a new dog
+*
+* @name: dog name
+*
+* @age: dog age
+*
+* @owner: dog owner
+*
+* Return: new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *cpname, *cpowner;
+	dog_t *newdog;
+
+	newdog = malloc(sizeof(dog_t));
+
+	if (newdog == NULL)
+		return (NULL);
+
+	cpname = malloc(sizeof(char) * _strlen(name));
+	cpowner = malloc(sizeof(char) * _strlen(owner));
+
+	if (cpname == NULL || cpowner == NULL)
+	{
+		free(newdog);
+		free(cpname);
+		free(cpowner);
+		return (NULL);
+	}
+
+	newdog->name = cpname;
+	newdog->age = age;
+	newdog->owner = cpowner;
+
+	return (newdog);
+}
