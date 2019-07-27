494ff4a869455155755c98deabf57323d2684549
danicelistobon <danielct-97@hotmail.com>
2019-07-27 19:55:08 +0000

    add 100

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,13 @@
 section .data
 	msg db "Hello, Holberton", 0x0a
-	len equ $ - msg
 section .text
 	global main
 main:
-	mov edx, len
+	mov edx, 17
 	mov ecx, msg
 	mov ebx, 1
 	mov eax, 4
 	int 0x80
 
 	mov eax, 1
-	int 0x80
+	int 0x80 
