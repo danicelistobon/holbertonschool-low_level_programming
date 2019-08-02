67b4d1c99feaabf4e05f5d3f1af8d542b9f06cc0
danicelistobon <danielct-97@hotmail.com>
2019-08-02 04:34:53 +0000

    add 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* print_list - returns the number of elements in a linked list_t list
+*
+* @h: list given
+*
+* Return: the number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
