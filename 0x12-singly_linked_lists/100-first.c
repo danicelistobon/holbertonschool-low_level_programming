c6c3b6ba42c4db1b11e4ae0c044771339e772d6d
danicelistobon <danielct-97@hotmail.com>
2019-08-02 05:36:03 +0000

    add 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+* firstprint - prints before main function
+*
+* Return: no return
+*/
+void firstprint(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
