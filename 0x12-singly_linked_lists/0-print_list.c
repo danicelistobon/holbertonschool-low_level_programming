dea2267b916f79db54d2667ba7301d9c15d3eecf
danicelistobon <danielct-97@hotmail.com>
2019-08-02 04:26:14 +0000

    add 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+* print_list - prints all the elements of a 'list_t' list
+*
+* @h: list given
+*
+* Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			i++;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+			i++;
+		}
+	}
+	return (i);
+}
