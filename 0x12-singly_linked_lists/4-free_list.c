80e486f7bebeae8f500be371a2517813da02194f
danicelistobon <danielct-97@hotmail.com>
2019-08-02 04:59:17 +0000

    add 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_list - frees a 'list_t' list
+*
+* @head: list
+*
+* Return: no return
+*/
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	free_list(head->next);
+
+	free(head->str);
+
+	free(head);
+}
