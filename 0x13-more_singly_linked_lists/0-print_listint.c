092036a0447ad235ea09868ceefde2f13057fac4
danicelistobon <danielct-97@hotmail.com>
2019-08-06 15:36:44 +0000

    add 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* print_listint - that prints all the elements of a listint_t list
+*
+* @h: list given
+*
+* Return: the number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
