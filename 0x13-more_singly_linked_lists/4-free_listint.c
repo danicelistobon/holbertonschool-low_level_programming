0d554ab4d28a1fe276eca0dbaa42f35a47f463de
danicelistobon <danielct-97@hotmail.com>
2019-08-06 19:16:17 +0000

    add 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_listint - frees a listint_t list
+*
+* @head: list
+*
+* Return: no return
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *first;
+
+	while (head != NULL)
+	{
+		first = head;
+		head = head->next;
+		free(first);
+	}
+}
